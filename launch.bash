890825b89cc173a98b7af397eaaf7ae9bb7ad921
glannuzel <glannuzel@ensc.fr>
2022-05-12 14:59:31 +0200

    Update launch.bash

--- launch.bash
+++ launch.bash
@@ -1,3 +1,12 @@
 pushd ~/dev/hello-world
+    systemctl --user stop reachy_teleop.service
+
+    status=$(systemctl --user is-active reachy_sdk_grpc.service)
+
+    if [ "$status" != "active" ]; then
+        systemctl --user restart reachy_sdk_grpc.service
+        sleep 30
+    fi
+
     python3 -m hello_world.hello
 popd
