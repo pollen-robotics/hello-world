a0a9629e926bdec91a444bc949479daa25738e53
glannuzel <gaelle.lannuzel@gmail.com>
2023-02-25 14:16:08 +0100

    Close #4.

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -19,6 +19,11 @@ if __name__ == '__main__':
     reachy = ReachySDK(host='localhost')
     logger.info('Connected to Reachy')
 
+    # Make sure that the torque are correctly set at 100, in case
+    # the previous turn_off_smoothly did not finish properly
+    for joint in reachy.joints.values():
+        joint.torque_limit = 100
+
     idle = Idle(name='idle', reachy=reachy)
 
     async def behavior():
