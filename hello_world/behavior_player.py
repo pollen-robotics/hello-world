74c695cfb87df6d4e1fc0e8a7f4bb9e6d4596f55
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-02-27 13:00:09 +0100

    Add Reachy IP address as an argument to use the script remotely.

--- hello_world/behavior_player.py
+++ hello_world/behavior_player.py
@@ -5,26 +5,12 @@ which plays behaviors randomly.
 This script might also be used to test behaviors that you have implemented yourself.
 """
 import asyncio
+from grpc._channel import _InactiveRpcError
 from reachy_sdk import ReachySDK
 from .behaviors.asleep import Asleep
 from .behaviors.look import LookHand
 from .behaviors.moods import Lonely, Tshirt, SweatHead, Sneeze, Whistle, Hello
 from .behaviors.scratch import Scratch
-
-
-reachy = ReachySDK(host='localhost')
-
-behavior_arg_to_class = {
-    'asleep': Asleep(name='asleep', reachy=reachy),
-    'look_hand': LookHand(name='look_hand', reachy=reachy),
-    'lonely': Lonely(name='lonely', reachy=reachy),
-    'scratch': Scratch(name='scratch', reachy=reachy),
-    'tshirt': Tshirt(name='tshirt', reachy=reachy),
-    'sweat_head': SweatHead(name='sweat_head', reachy=reachy),
-    'sneeze': Sneeze(name='sneeze', reachy=reachy),
-    'whistle': Whistle(name='whistle', reachy=reachy),
-    'hello': Hello(name='hello', reachy=reachy),
-}
 
 
 def main():
@@ -45,8 +31,30 @@ def main():
             'hello',
         ]
     )
+    parser.add_argument('--ip_address', help="Reachy's ip address, default is 'localhost'.", default='localhost')
+
     args = parser.parse_args()
     requested_behavior = args.behavior
+    reachy_ip_address = args.ip_address
+
+    try:
+        reachy = ReachySDK(host=reachy_ip_address)
+    except _InactiveRpcError:
+        print('Could not connect to Reachy. \n \
+Make sure that reachy_sdk_server.service is running and that you entered the correct IP address.')
+        exit()
+
+    behavior_arg_to_class = {
+        'asleep': Asleep(name='asleep', reachy=reachy),
+        'look_hand': LookHand(name='look_hand', reachy=reachy),
+        'lonely': Lonely(name='lonely', reachy=reachy),
+        'scratch': Scratch(name='scratch', reachy=reachy),
+        'tshirt': Tshirt(name='tshirt', reachy=reachy),
+        'sweat_head': SweatHead(name='sweat_head', reachy=reachy),
+        'sneeze': Sneeze(name='sneeze', reachy=reachy),
+        'whistle': Whistle(name='whistle', reachy=reachy),
+        'hello': Hello(name='hello', reachy=reachy),
+    }
 
     # Make sure that the torque are correctly set at 100, in case
     # the previous turn_off_smoothly did not finish properly
